ff278a03eaf225fe21ba1d12193531a02754f0bd
kevinbaranowski <72942742+kevinbaranowski@users.noreply.github.com>
2024-05-28 11:26:17 -0500

    task 4 completed. tests passing

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -6,11 +6,43 @@ namespace TechJobs.Tests
 	{
         //Testing Objects
         //initalize your testing objects here
+        Job job1;
+        Job job2;
+        Job job3;
+        Job job4;
+        [TestInitialize]
+        public void CreateJobObjects()
+        {
+            job1 = new Job();
+
+            job2 = new Job();
+
+            job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        }
+        
 
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobId()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            //Assert.IsFalse(job1.Equals(job2));
+            Assert.IsFalse(job1.Id == job2.Id);
+            Assert.IsTrue(job2.Id - job1.Id == 1);
+        }
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Assert.AreEqual("Product tester", job3.Name);
+            Assert.AreEqual("ACME", job3.EmployerName.Value);
+            Assert.AreEqual("Desert", job3.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", job3.JobType.Value);
+            Assert.AreEqual("Persistence", job3.JobCoreCompetency.Value);
+        }
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Assert.IsFalse(job1.Equals(job2));
         }
     }
 }
--- TechJobsOOAutoGraded6/Job.cs
+++ TechJobsOOAutoGraded6/Job.cs
@@ -3,7 +3,7 @@ namespace TechJobsOOAutoGraded6
 {
 	public class Job
 	{
-        /*
+        
 
             public int Id { get; }
             private static int nextId = 1;
@@ -14,14 +14,39 @@ namespace TechJobsOOAutoGraded6
             public CoreCompetency JobCoreCompetency { get; set; }
 
             // TODO: Task 3: Add the two necessary constructors.
+            public Job()
+            {
+                Id = nextId;
+                nextId++;
+            }
 
-            // TODO: Task 3: Generate Equals() and GetHashCode() methods.  
+            public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+            {
+                Name = name;
+                EmployerName = employerName;
+                EmployerLocation = employerLocation;
+                JobType = jobType;
+                JobCoreCompetency = jobCoreCompetency;
+            }
+        // TODO: Task 3: Generate Equals() and GetHashCode() methods.
+        public override bool Equals(object? obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
 
-            // TODO: Task 5: Generate custom ToString() method.
-                //Until you create this method, you will not be able to print a job to the console.
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+          
+
+        // TODO: Task 5: Generate custom ToString() method.
+        //Until you create this method, you will not be able to print a job to the console.
 
 
-        */
+
     }
 }
 
